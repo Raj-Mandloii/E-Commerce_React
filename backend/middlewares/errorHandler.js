0c78c9fa5f177071d5654c7578084c0e8b11668c
Raj Mandloii <rajmandloi01.01@gmail.com>
2023-01-13 14:44:34 +0530

    error handlers middleware added

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,15 @@
+// Invalid URLS
+
+const notFound = (req, res) => {
+  const error = new Error(`Not Found : ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode == 200 ? 500 : res.statusCode;
+  req.status(statusCode);
+  res.json({ message: err?.message, stack: err?.stack });
+};
+
+module.exports = { errorHandler, notFound };
