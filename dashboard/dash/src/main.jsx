c6a1791246d4eafe15eadef2845d2daf5a6a588b
Raj Mandloii <rajmandloi01.01@gmail.com>
2023-01-29 20:46:57 +0530

    chakra theme added

--- dashboard/dash/src/main.jsx
+++ dashboard/dash/src/main.jsx
@@ -3,12 +3,21 @@ import * as ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { extendTheme } from "@chakra-ui/react";
+const colors = {
+  custom_button: {
+    900: "red",
+    800: "red",
+    700: "red",
+  },
+};
 
+const theme = extendTheme({ colors });
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </BrowserRouter>
